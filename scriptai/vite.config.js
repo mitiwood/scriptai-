d501e8ec13c6472f878c75ee5e6aef826251ad65
kenny <44049939+mitiwood@users.noreply.github.com>
2026-02-27 05:33:28 +0900

    add api route

--- scriptai/vite.config.js
+++ scriptai/vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./", // 이 부분을 추가하여 경로를 상대 경로로 바꿉니다.
+});
